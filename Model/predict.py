86fda2500bb6c1653ad67a91bb108f0b6e61e4d6
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-04-06 08:18:28 +0545

    Update predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -1,7 +1,9 @@
+import os
 import pickle
-import pandas as pd
-with open("Model\model (3).pkl","rb") as f:
-    model=pickle.load(f)
+base_path = os.path.dirname(__file__)
+model_path = os.path.join(base_path, "model.pkl")
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 def predict_output(user_input:dict):
     input_df=pd.DataFrame([user_input])
     output=model.predict(input_df)[0]
