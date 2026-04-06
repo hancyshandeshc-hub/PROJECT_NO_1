6859f26a0aa69aed1463d3e28361055e03be40a8
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-04-06 08:26:19 +0545

    Update predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -1,7 +1,6 @@
 import os
 import pickle
-base_path = os.path.dirname(__file__)
-model_path = os.path.join(base_path, "model.pkl")
+model_path = os.path.join("Model", "model.pkl")
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 def predict_output(user_input:dict):
