731af347e150a49bb73bf86cea9cc058e167213d
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-09-20 13:44:36 +0545

    Refactor predict.py for improved readability

--- Model/predict.py
+++ Model/predict.py
@@ -1,10 +1,18 @@
 import os
 import pickle
 import pandas as pd
+
+
 model_path = os.path.join("Model", "model (3).pkl")
+
 with open(model_path, "rb") as f:
     model = pickle.load(f)
-def predict_output(user_input:dict):
-    input_df=pd.DataFrame([user_input])
-    output=model.predict(input_df)[0]
+
+
+def predict_output(user_input: dict):
+
+    input_df = pd.DataFrame([user_input])
+
+    output = model.predict(input_df)[0]
+
     return output
