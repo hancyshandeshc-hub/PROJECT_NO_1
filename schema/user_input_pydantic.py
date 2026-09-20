af328e0771a60ff17152200fbd2b850b9a428125
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-09-20 13:44:06 +0545

    Refactor UserInput model with new fields

--- schema/user_input_pydantic.py
+++ schema/user_input_pydantic.py
@@ -1,33 +1,13 @@
-from pydantic import BaseModel, Field, computed_field, field_validator
-from typing import Literal, Annotated
-class UserInput(BaseModel):
-	name:Annotated[str,Field(
-							...,
-               				description="Name of the Car",
-							)]
+from pydantic import BaseModel
 
-	company:Annotated[str,Field(
-							...,
-               				description="Name of the Company",
-							)]
-	year:Annotated[int,Field(
-							...,
-               				gt=0,
-							description="When was the car purchased",
-							)]
-	kms_driven:Annotated[int,Field(
-							...,
-               				gt=0,
-							description="Kilometers Driven by the Car",
-							)]	
-	@field_validator("name")
-	@classmethod
-	def normalize_feature1(cls,v:str)->str:
-		v=v.strip().title()
-		return v
-	@field_validator("company")
-	@classmethod
-	def normalize_feature2(cls,v:str)->str:
-		v=v.strip().title()
-		return v
-	
+
+class UserInput(BaseModel):
+
+    MONTH: int
+    DISTRICT: str
+    RH2M: float
+    T2M: float
+    WS10M: float
+    PS: float
+    PRECTOT_LAST_MONTH: float
+    RH2M_LAST_MONTH: float
